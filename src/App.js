65fde8efda75248bb03c7d2c1d982c5cc762da20
marcomaciel04 <marco.maciel@csacademy.com.br>
2024-07-23 12:07:19 -0300

    API Change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/items');
+        const response = await axios.get('https://api-churn.onrender.com/api/items');
         setItems(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
