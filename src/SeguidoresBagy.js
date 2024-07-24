292a4a3e614efe9fd26ee47b9828667d1966ca77
marcomaciel04 <marco.maciel@csacademy.com.br>
2024-07-24 16:38:17 -0300

    Novos Campos

--- src/SeguidoresBagy.js
+++ src/SeguidoresBagy.js
@@ -25,9 +25,17 @@ const SeguidoresBagy = () => {
           <tr>
             <th>Username</th>
             <th>URL</th>
-            <th>Data de Registro</th>
-            <th>Usuário Novo</th>
             <th>Link da Bio</th>
+            <th>Bio</th>
+            <th>Seguindo</th>
+            <th>Seguidores</th>
+            <th>Stories Públicos</th>
+            <th>Conta Comercial</th>
+            <th>Privado</th>
+            <th>Verificado</th>
+            <th>Contagem de Mídias</th>
+            <th>Data de Registro</th>
+            <th>Seguidor Novo</th>
           </tr>
         </thead>
         <tbody>
@@ -35,9 +43,17 @@ const SeguidoresBagy = () => {
             <tr key={follower.username}>
               <td>{follower.username}</td>
               <td><a href={follower.user_url} target="_blank" rel="noopener noreferrer">{follower.user_url}</a></td>
+              <td><a href={follower.bio_link} target="_blank" rel="noopener noreferrer">{follower.bio_link}</a></td>
+              <td>{follower.bio}</td>
+              <td>{follower.followees}</td>
+              <td>{follower.followers}</td>
+              <td>{follower.has_public_story ? 'Sim' : 'Não'}</td>
+              <td>{follower.is_business_account ? 'Sim' : 'Não'}</td>
+              <td>{follower.is_private ? 'Sim' : 'Não'}</td>
+              <td>{follower.is_verified ? 'Sim' : 'Não'}</td>
+              <td>{follower.media_count}</td>
               <td>{follower.register_date}</td>
               <td>{follower.is_new_user ? 'Sim' : 'Não'}</td>
-              <td><a href={follower.bio_link} target="_blank" rel="noopener noreferrer">{follower.bio_link}</a></td>
             </tr>
           ))}
         </tbody>
